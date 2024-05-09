9ada508c52a417b80b9ae3c1890a44a433f8f222
Eemeli Oksanen <eemelioksanen@protonmail.com>
2024-05-09 21:19:50 +0300

    init commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+
+import vercel from '@astrojs/vercel/static'
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  output: 'static',
+  adapter: vercel({
+    imageService: true,
+  }),
+})
